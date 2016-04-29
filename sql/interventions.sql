8bdf8c98a023091d7988934e7ddb28d890f5dff1
Ouwen <ouwen.huang@hotmail.com>
2016-04-29 17:27:32 -0400

    Added constraint for trigger parsing.

--- sql/interventions.sql
+++ sql/interventions.sql
@@ -1,8 +1,21 @@
+CREATE OR REPLACE FUNCTION parsable(trigger text)
+  RETURNS boolean AS
+	$BODY$
+		var self = {};
+		try {
+		    eval(trigger);
+		} catch (e) {
+		    return false;
+		}
+		return true;
+	$BODY$
+  LANGUAGE plv8;
+
 CREATE TABLE interventions (
   preop_variable text PRIMARY KEY REFERENCES patient_variables_metadata (variable_name),
   label text NOT NULL,
   active boolean NOT NULL, 
-  trigger text, -- how to parse?
+  trigger text, CHECK (parsable(trigger)),
   phase text NOT NULL, CHECK (phase IN ('postop', 'preop')),
   context text NOT NULL, CHECK (phase IN ('risk_factor', 'outcome')),
   description text
