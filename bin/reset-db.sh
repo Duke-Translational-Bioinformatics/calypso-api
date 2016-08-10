396cdb667e301534e7e0c3730c3f8e9a9c835f38
Keegan Brown <brown.keegan@gmail.com>
2016-08-10 16:06:47 -0500

    adds deploy scripts

---
+++ bin/reset-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Dropping and recreating calypso_test"
+psql -w -U postgres -c "DROP DATABASE calypso_test"
+psql -w -U postgres -c "CREATE DATABASE calypso_test"
+# psql -w -U postgres -c 'CREATE EXTENSION plv8'
+# psql -w -U postgres -c 'CREATE EXTENSION plls'
+# psql -w -U postgres -c 'CREATE EXTENSION plcoffee'
+echo "Running through ../sql/run.sql"
+cd ../sql
+while read sqlline
+do
+    psql -w -U postgres -d calypso_test -f "${sqlline}"
+done < run.sql
---
+++ bin/sb-staging-deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh deployer@calypso-qa.sbox.es 'cd calypso-api && git fetch --all && git reset --hard origin/feature/csv-parser && pm2 restart all'
