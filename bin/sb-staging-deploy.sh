a5ada2249b018c9982783c3ab3574edd8a2335b4
Keegan Brown <brown.keegan@gmail.com>
2016-08-10 16:24:14 -0500

    deploy script working

--- bin/sb-staging-deploy.sh
+++ bin/sb-staging-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh deployer@calypso-qa.sbox.es 'cd calypso-api && git fetch --all && git reset --hard origin/feature/csv-parser && pm2 restart pm2-run-api.sh'
+ssh deployer@calypso-qa.sbox.es 'cd calypso-api && git fetch --all && git reset --hard origin/feature/csv-parser && pm2 restart pm2-run-api'
