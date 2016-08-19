69bf38d4e01f4138a2f94472f1e5dcede809045c
Keegan Brown <brown.keegan@gmail.com>
2016-08-19 16:00:50 -0500

    add logs to deploy script

--- bin/sb-staging-deploy.sh
+++ bin/sb-staging-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh deployer@calypso-qa.sbox.es 'export NODE_ENV=test && cd calypso-api && git fetch --all && git reset --hard origin/feature/csv-parser && npm install && pm2 restart pm2-run-api'
+ssh deployer@calypso-qa.sbox.es 'export NODE_ENV=test && cd calypso-api && git fetch --all && git reset --hard origin/feature/csv-parser && npm install && pm2 restart pm2-run-api && pm2 logs pm2-run-api --lines 100'
