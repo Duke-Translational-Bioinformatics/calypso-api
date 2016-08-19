8ce011445a860776883d4932a91ae12f096e0cbc
Keegan Brown <brown.keegan@gmail.com>
2016-08-19 15:28:58 -0500

    fix db reset

--- bin/reset-db.sh
+++ bin/reset-db.sh
@@ -6,11 +6,7 @@ psql -w -U postgres -c "CREATE DATABASE calypso_test"
 # psql -w -U postgres -c 'CREATE EXTENSION plls'
 # psql -w -U postgres -c 'CREATE EXTENSION plcoffee'
 echo "Running through ../sql/run.sql"
-<<<<<<< HEAD
-cd ../sql
-=======
 cd sql
->>>>>>> 4afcd73c3a72862795df9d9a63d060531a7b8bee
 while read sqlline
 do
     psql -w -U postgres -d calypso_test -f "${sqlline}"
